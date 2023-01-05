8ae7c6227deea20ff642db89abfc888c348c6d3d
IVashty <vashtylavilavi50@gmail.com>
2023-01-05 08:55:03 +0300

    setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
 -- my setup for sql enviroment
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
