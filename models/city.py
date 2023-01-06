8443872006a7dddb6dbcd53866d24bfed3bba70e
IVashty <vashtylavilavi50@gmail.com>
2023-01-06 09:50:07 +0300

    models/city.py

--- models/city.py
+++ models/city.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, create_engine, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String,ForeignKey
+from os import getenv
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    """
+    The city class, contains state ID and name available to the users
+    """
+     if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
