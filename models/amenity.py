9157d8d2fe5751fcc0f5ee99020d5b6e2abad6bb
IVashty <vashtylavilavi50@gmail.com>
2023-01-06 08:53:43 +0300

    models/amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, Integer, String
 
 
 class Amenity(BaseModel):
-    name = ""
+    """
+    Represents Amenities available to users
+    """
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
