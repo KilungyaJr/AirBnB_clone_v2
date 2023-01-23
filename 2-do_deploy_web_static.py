3c3b70a95dc1967f279813b99b0a7a0f3e2122a9
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-01-23 15:41:27 +0300

    deploys archive

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+from datetime import datetime
+from fabric.api import env, local, put, run, settings
+
+env.hosts = ['52.91.121.190', '100.26.168.135']
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder
+    of the AirBnB Clone repo
+    """
+    # Create versions folder if it doesn't exist
+    local("mkdir -p versions")
+
+    # Get the current timestamp
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Create the archive file name
+    archive_name = "web_static_{}.tgz".format(timestamp)
+
+    # Create the archive
+    local("tar -czvf versions/{} web_static".format(archive_name))
+
+    # Check if the archive was created successfully
+    if local("ls versions/{}".format(archive_name), capture=True).succeeded:
+        return "versions/{}".format(archive_name)
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to your web servers
+    """
+    if not local("test -e {}".format(archive_path), capture=True).succeeded:
+        return False
+
+    archive_name = archive_path.split("/")[-1]
+    release_folder = "/data/web_static/releases/{}".format(archive_name.split(".")[0])
+
+    with settings(warn_only=True):
+        # Upload archive to /tmp/ on the web server
+        put(archive_path, "/tmp/")
+
+        # Uncompress archive to releases folder
+        run("mkdir -p {}".format(release_folder))
+        run("tar -xzf /tmp/{} -C {}".format(archive_name, release_folder))
+
+        # Delete archive from web server
+        run("rm /tmp/{}".format(archive_name))
+
+        # Delete and recreate the symbolic link
+        run("rm -f /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(release_folder))
+    print('New version deployed!')
+    return True
