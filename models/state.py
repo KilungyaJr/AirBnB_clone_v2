cf9546c6a496c07892e6d8ca0b434ff682c9baca
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-02-04 15:27:14 +0300

    updated string format

--- models/state.py
+++ models/state.py
@@ -2,7 +2,7 @@
 """ State Module for HBNB project """
 import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
 from models.city import City
 from os import getenv
@@ -10,7 +10,7 @@ from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
