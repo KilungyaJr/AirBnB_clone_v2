fdd2d47869ba9666816a15e60f67e2917c86f7fc
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-02-06 17:00:02 +0300

    updated instantiation

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage/DBStorage"""
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 import os
 
 if os.getenv("HBNB_TYPE_STORAGE") == "db":
@@ -11,4 +18,3 @@ else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
     storage.reload()
-    Base = {}
